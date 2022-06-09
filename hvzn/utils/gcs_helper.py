e3280b4ac8d72f8997b72234ea23e6be6f1dcbcf
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2022-06-09 06:39:36 +0000

    add delete file function, gcshelper to class

--- hvzn/utils/gcs_helper.py
+++ hvzn/utils/gcs_helper.py
@@ -2,18 +2,21 @@ import gcsfs
 import pickle
 
 
-def save_pkl_to_gcs(gcs_path, object):
-    fs = gcsfs.GCSFileSystem()
-    with fs.open(gcs_path, "wb") as f:
-        pickle.dump(object, f)
+class GcsHelper:
+    def __init__(self) -> None:
+        self.fs = gcsfs.GCSFileSystem()
 
+    def save_pkl_to_gcs(self, gcs_path, object):
+        with self.fs.open(gcs_path, "wb") as f:
+            pickle.dump(object, f)
 
-def read_pkl_from_gcs(gcs_path):
-    fs = gcsfs.GCSFileSystem()
-    with fs.open(gcs_path, "rb") as f:
-        return pickle.load(f)
+    def read_pkl_from_gcs(self, gcs_path):
+        with self.fs.open(gcs_path, "rb") as f:
+            return pickle.load(f)
 
+    def check_gcs_exist(self, gcs_path) -> bool:
+        return self.fs.exists(gcs_path)
 
-def check_gcs_exist(gcs_path) -> bool:
-    fs = gcsfs.GCSFileSystem()
-    return fs.exists(gcs_path)
+    def delete_gcs_file(self, gcs_path) -> bool:
+        self.fs.rm(gcs_path)
+        return self.check_gcs_exist(gcs_path) == False
--- tests/test_gcs_helper.py
+++ tests/test_gcs_helper.py
@@ -1,18 +1,23 @@
 import numpy as np
 import pickle
-from hvzn.utils.gcs_helper import save_pkl_to_gcs, read_pkl_from_gcs, check_gcs_exist
+from hvzn.utils import GcsHelper
 
 
 arr = np.array([0, 1, 2, 3, 4, 4, 4, 4, 6, 7, 8])
+gcs_path = "gs://hvzn-gcs-us-1/tests/pytest_array.pkl"
+gcs = GcsHelper()
 
 
 def test_save_pkl():
-    gcs_path = "gs://hvzn-gcs-us-1/tests/array_test2.pkl"
-    save_pkl_to_gcs(gcs_path=gcs_path, object=arr)
-    assert check_gcs_exist(gcs_path) == True
+    gcs.save_pkl_to_gcs(gcs_path=gcs_path, object=arr)
+    assert gcs.check_gcs_exist(gcs_path) == True
 
 
 def test_load_pkl():
-    gcs_path = "gs://hvzn-gcs-us-1/tests/array_test2.pkl"
-    loaded_array = read_pkl_from_gcs(gcs_path=gcs_path)
+    loaded_array = gcs.read_pkl_from_gcs(gcs_path=gcs_path)
     assert np.array_equal(arr, loaded_array) == True
+
+
+def test_delete_file():
+    gcs.delete_gcs_file(gcs_path)
+    assert gcs.check_gcs_exist(gcs_path) == False
