e916ce20f4436c93e87828a974c65fac21316b3b
afif2100 <32302970+afif2100@users.noreply.github.com>
2023-02-08 20:09:07 +0700

    fix some files

--- ps-sentiment-scrapper/get-data-ingestion-predict.py
+++ ps-sentiment-scrapper/get-data-ingestion-predict.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     preds = SentimentPredictor()
 
     # insert data
-    app_ids = ["id.co.bri.brimo", "com.bca", "id.bmri.livin", "net.myinfosys.PermataMobileX"]
+    app_ids = ["id.co.bri.brimo", "com.bca", "id.bmri.livin", "net.myinfosys.PermataMobileX", "id.co.btn.mobilebanking.android"]
     for app in app_ids:
         # Get App review and insert to LocalDB
         get_review_and_insert(
