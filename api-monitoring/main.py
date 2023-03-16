823ca6f419964129bc70b7850c41f6df083927af
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2023-03-16 14:27:10 +0700

    add metrics collector

--- api-monitoring/main.py
+++ api-monitoring/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
+import numpy as np
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
@@ -9,6 +10,12 @@ metrics = PrometheusMetrics(app)
 metrics.info("app_info", "Application info", version="1.0.3")
 
 
-@app.route("/")
-def index():
-    return {"hello": "world"}
+@app.route("/health")
+def health():
+    return {"health": "OK"}
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    result_ = np.random.randint(1, 100)
+    return {"result": result_}
