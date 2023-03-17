491b4cb6d5709db7d1fe36c55e0341b255a61fc1
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2023-03-17 08:50:20 +0700

    update mainpy

--- api-monitoring/main.py
+++ api-monitoring/main.py
@@ -1,26 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 import numpy as np
-from time import sleep
+import asyncio
 from loguru import logger
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-
 
 # static information as metric
 metrics.info("app_info", "Application info", version="1.0.3")
 
 
 @app.route("/health")
-def health():
+async def health():
     return {"health": "OK"}
 
 
 @app.route("/predict", methods=["POST"])
-def predict():
-    rand_nums = np.random.uniform(low=0.001, high=0.2, size=(1,))[0]
-    rand_nums = round(rand_nums,4)
+async def predict():
+    rand_nums = np.random.uniform(low=0.001, high=0.3, size=(1,))[0]
+    rand_nums = round(rand_nums, 4)
     logger.info(f"Sleep for {rand_nums}s")
-    sleep(rand_nums)
+    await asyncio.sleep(float(rand_nums))
     return {"result": str(rand_nums)}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port="5000")
